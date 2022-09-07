6ed1878835ad08f81bb75a201e4f2a91a6bf810e
Michiel Dijk <michiellof@gmail.com>
2022-09-07 09:18:23 +0200

    Pokedex project

--- 03_Props_and_more/04_JSX_Loops/Friend.js
+++ 03_Props_and_more/04_JSX_Loops/Friend.js
@@ -1,16 +1,16 @@
 class Friend extends React.Component {
-    render() {
-        const { name, hobbies } = this.props;
+  render() {
+    const { name, hobbies } = this.props;
 
-        return (
-            <div>
-                <h1>{name}</h1>
-                <ul>
-                    {hobbies.map(h => <li>{h}</li>)}
-                </ul>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <h1>{name}</h1>
+        <ul>
+          {hobbies.map(h => <li>{h}</li>)}
+        </ul>
+      </div>
+    );
+  }
 }
 
 // CODE EXAMPLES
