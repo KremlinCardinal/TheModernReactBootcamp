83cfe56c0db6705665da107154ad7323d9799f2a
Michiel Dijk <michiellof@gmail.com>
2022-12-16 07:54:05 +0100

    Added some comments

--- 06_Introducing_State/state_clicker/src/Clicker.js
+++ 06_Introducing_State/state_clicker/src/Clicker.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 class Clicker extends Component {
   constructor(props) {
     super(props);
+
+    //Initializes and sets the default value of the "num" state
     this.state = {
       num: 1
     }
@@ -10,6 +12,7 @@ class Clicker extends Component {
     this.generateNumber = this.generateNumber.bind(this);
   }
 
+  //Generates random number between 1 and 10 and binds it to the "num" state
   generateNumber(e) {
     let rand = Math.floor(Math.random() * 10) + 1;
     this.setState({ num: rand });
@@ -19,6 +22,7 @@ class Clicker extends Component {
     return (
       <div>
         <h1>Number is {this.state.num}</h1>
+        {/* Checks if random number is 7 */}
         {this.state.num === 7
           ? <h2>YOU WIN!</h2>
           : <button onClick={this.generateNumber}>Random Number</button>
